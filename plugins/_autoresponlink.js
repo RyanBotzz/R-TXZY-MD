a6aa6640f00924626169462f9831dbdd097a654e
RyanBotzz <109885043+RyanBotzz@users.noreply.github.com>
2022-08-03 20:42:07 +0700

    Update _autoresponlink.js

--- plugins/_autoresponlink.js
+++ plugins/_autoresponlink.js
@@ -6,7 +6,8 @@ handler.all = async function (m, { isBlocked }) {
     if ((m.mtype === 'groupInviteMessage' || m.text.startsWith('Undangan untuk bergabung') || m.text.startsWith('Invitation to join') || m.text.startsWith('Buka tautan ini')) && !m.isBaileys && !m.isGroup) {
     let teks = `Invite Group
 • 15 Day / Rp 5k
-• 30 Day / Rp 10k
+• 30 Day / Rp 15k
+• Permanen / Rp 20k
 Jika berminat hubungi: @${global.owner[0]} untuk order:)
 `
     this.reply(m.chat, teks, m)
